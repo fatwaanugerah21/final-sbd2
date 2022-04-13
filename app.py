30b95a2d56724e6e250041432a4807f3a1970969
fatwaanugerah21 <fatwaanugerah0421@gmail.com>
2022-04-13 23:30:33 +0800

    selesai sampai pagination

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from crypt import methods
-from pickletools import read_uint1
-from flask import Flask, jsonify, render_template, request
+
+
+from flask import Flask, jsonify, redirect, render_template, request
 
 from models import user
 from models.news import News
@@ -10,74 +10,105 @@ app = Flask(__name__)
 user_model = user.User()
 news_model = News()
 
+
+def getTrimmedWords(word):
+    texts = word.split()
+
+    text = ""
+    lastRange = min(len(texts), 20)
+    for i in range(0, lastRange):
+        text += texts[i] + " "
+
+    return text
+
+
 @app.route("/")
 def index():
-  return render_template("index.html")
+    return redirect("/news")
 
-# Untuk login
-@app.route("/login", methods=["GET", "POST"])
-def login():
-  if request.method == "GET":
-    return render_template("login.html")
-
-  if request.method == "POST":
-    username = request.form.get("username")
-    password = request.form.get("password")
-
-    users = user_model.find({"username": username, "password": password})
-    if (len(users) < 1):
-      return "User tidak ditemukan"
-    
-    user = users[0]
-    return "Selamat datang " + user["username"]
-
-# Untuk daftar user
-@app.route("/signup", methods=["GET", "POST"])
-def signup():
-  if request.method == "GET":
-    return render_template("signup.html")
-
-  if request.method == "POST":
-    firstname = request.form.get("firstname")
-    lastname = request.form.get("lastname")
-    username = request.form.get("username")
-    password = request.form.get("password")
-
-    user_model.create({
-      "firstname": firstname,
-      "lastname": lastname,
-      "username": username,
-      "password": password,
-    })
-
-    return "User berhasil dibuat"
-
-# Untuk input berita
-@app.route("/news/create", methods=["GET", "POST"])
-def create_news():
-  if request.method == "GET":
-    return render_template("create-news.html")
-
-  if request.method == "POST":
-    title = request.form.get("title")
-    writer = request.form.get("writer")
-    text = request.form.get("text")
-    category = request.form.get("category")
-
-    newNewsData = {
-      "title": title,
-      "writer": writer,
-      "text": text,
-      "category": category,
-      "readed": 0
-    }
-    news_model.create(newNewsData)
-
-    return "Success ditambahkan"
 
 @app.route("/news")
 def news():
-  news = news_model.find({}, start=0)
-  return jsonify(news)
+    startArgs = request.args.get("start")
+
+    start = 0 if startArgs == None else int(startArgs)
+    limit = 5
+    news = news_model.find({}, start=start * limit, limit=limit)
+
+    for n in news["data"]:
+        currentText = n["text"]
+        n["text"] = getTrimmedWords(currentText)
+
+    hasMore = news["pagination"]["hasMore"]
+
+    return render_template("index.html", news=news["data"], start=start, hasMore=hasMore)
+
+# Untuk login
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        users = user_model.find({"username": username, "password": password})
+        if (len(users) < 1):
+            return "User tidak ditemukan"
+
+        user = users[0]
+        return "Selamat datang " + user["username"]
+
+# Untuk daftar user
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+
+    if request.method == "POST":
+        firstname = request.form.get("firstname")
+        lastname = request.form.get("lastname")
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user_model.create({
+            "firstname": firstname,
+            "lastname": lastname,
+            "username": username,
+            "password": password,
+        })
+
+        return "User berhasil dibuat"
+
+# Untuk input berita
+
+
+@app.route("/news/create", methods=["GET", "POST"])
+def create_news():
+    if request.method == "GET":
+        return render_template("create-news.html")
+
+    if request.method == "POST":
+        title = request.form.get("title")
+        writer = request.form.get("writer")
+        text = request.form.get("text")
+        category = request.form.get("category")
+
+        newNewsData = {
+            "title": title,
+            "writer": writer,
+            "text": text,
+            "category": category,
+            "readed": 0
+        }
+        news_model.create(newNewsData)
+
+        return "Success ditambahkan"
+
 
 app.run(debug=True)
--- models/news.py
+++ models/news.py
@@ -3,6 +3,7 @@ from factory.database import Database
 from config import config
 from functions import convert_id
 
+
 class News(object):
     def __init__(self):
         self.validator = Validator()
@@ -20,46 +21,52 @@ class News(object):
             "updated": "datetime",
         }
 
-        self.create_required_fields = ["title", "writer", "text", "category", "readed"]
+        self.create_required_fields = [
+            "title", "writer", "text", "category", "readed"]
 
         # Fields optional for CREATE
         self.create_optional_fields = []
 
         # Fields required for UPDATE
-        self.update_required_fields = ["title", "writer", "text", "category", "readed"]
+        self.update_required_fields = [
+            "title", "writer", "text", "category", "readed"]
 
         # Fields optional for UPDATE
         self.update_optional_fields = []
 
     def create(self, news):
         # Validator will throw error if invalid
-        self.validator.validate(news, self.fields, self.create_required_fields, self.create_optional_fields)
+        self.validator.validate(
+            news, self.fields, self.create_required_fields, self.create_optional_fields)
         res = self.db.insert(news, self.collection_name)
         return "Inserted Id " + res
 
-    def find(self, filter, start, limit = config["news_limit"]):
+    def find(self, filter, start, limit=config["news_limit"]):
         # skip & limit is for pagination
         # limit + 1 is for checking hasMore
-        data = self.db.find(filter, self.collection_name, cursor=True).skip(start).limit(limit)
+        data = self.db.find(filter, self.collection_name,
+                            cursor=True).skip(start).limit(limit).sort("created", -1)
         data = convert_id(data)
 
         # Pagination stuff
         count = len(data)
-        nextData = self.db.find(filter, self.collection_name, skip=start + limit, limit=1)
+        nextData = self.db.find(
+            filter, self.collection_name, skip=start + limit, limit=1)
         hasMore = len(nextData) >= 1
         pagination = {
             "count": count,
             "hasMore": hasMore
         }
 
-        return { "data": data, "pagination": pagination }
+        return {"data": data, "pagination": pagination}
 
     def find_by_id(self, id):
         return self.db.find_by_id(id, self.collection_name)
 
     def update(self, id, news):
-        self.validator.validate(news, self.fields, self.update_required_fields, self.update_optional_fields)
-        return self.db.update(id, news,self.collection_name)
+        self.validator.validate(
+            news, self.fields, self.update_required_fields, self.update_optional_fields)
+        return self.db.update(id, news, self.collection_name)
 
     def delete(self, id):
         return self.db.delete(id, self.collection_name)
